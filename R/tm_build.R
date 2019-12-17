8a69aee1b8edc6ff0fd142fc627a0d83c47bc1f4
R.Andres Castaneda <acastanedaa@worldbank.org>
2019-12-16 20:37:01 -0500

    basic strcuture of tm build

---
+++ R/tm_build.R
@@ -0,0 +1,27 @@
+#' Builds list of data sets for calculations
+#'
+#' @param country
+#' @param year
+#' @param survey
+#' @param vermast
+#' @param veralt
+#' @param type
+#' @param maindir
+#' @param drive
+#'
+#' @return
+#' @export
+#'
+#' @examples
+tm_build <- function(country,
+                     year = NA,
+                     survey = NA,
+                     vermast = NA,
+                     veralt = NA,
+                     type = "PX",
+                     maindir = ":/03.ProjectX/data",
+                     drive = "p") {
+
+# check that vectors are the same size.
+
+}
