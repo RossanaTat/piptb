bc04a27c0682c804823582bf9a0cb0fc0279aa07
R.Andres Castaneda <acastanedaa@worldbank.org>
2020-01-14 10:07:07 -0500

    add several statistics

--- R/tm_DT_output.R
+++ R/tm_DT_output.R
@@ -43,19 +43,9 @@ tm_DT_output <- function(countries,
                          calc_vars    = "pov_status",
                          stats        = "mean") {
 
-
+  # Load data
   DT <- tm_build_DT(countries = countries,
                  years = years)
-
-  # remove poverty status in case it is selected
-  calc_vars_npov <- calc_vars[!calc_vars %in% "pov_status"]
-
-
-  # Selected variables and final variables to make DT smaller
-
-  svars  <- c(colvar, rowvar, super_colvar, super_rowvar, calc_vars_npov)
-  fvars <- unique(c("weight", "welfare", "welfare_ppp",  "surveyid", svars))
-  DT <- DT[,eval(fvars)]
 
   # Poverty status
   for (i in povlines) {
@@ -63,16 +53,25 @@ tm_DT_output <- function(countries,
     DT[, (nvar) :=  welfare_ppp < i ]
   }
 
+  # Expand poverty status for all povlines in case it is selected
+  if ("pov_status"  %in% calc_vars) {
+    calc_vars <- calc_vars[!calc_vars %in% "pov_status"]
+    calc_vars <- c(calc_vars, grep( "^pov_status", names(DT), value = TRUE))
+  }
 
-  # calculation variables
-  cv <- paste(as.character(calc_vars), collapse  = "|")
+
+  # Selected and final variables to make DT smaller
+  svars  <- c(colvar, rowvar, super_colvar, super_rowvar, calc_vars) # Selected vars
+  fvars <- unique(c("weight", "welfare", "welfare_ppp",  "surveyid", svars)) # final vars
+  DT <- DT[, ..fvars]
+
 
   #--------- Summarise several columns by group
 
   # get variable by which data will be filtered
   keyby_var <- "list(surveyid"
   new_names <- NULL
-  nk <- 1    # starting point. [1] == surveyid
+  nk <- 1    # counter. [1] == surveyid
 
 
   if (length(colvar) == 1) {
@@ -102,14 +101,35 @@ tm_DT_output <- function(countries,
   keyby_text <- parse(text = keyby_var)
 
 
-  # parse in Data Table
+  #--------- Lines of estimation
 
+  stats_text <- NULL
+  for (i in seq_along(stats)) {
+    if (stats[i] == "mean") {
+      stats_text <- paste("lapply(.SD, weighted.mean, w = weight , na.rm = TRUE)",
+                          stats_text, sep = ",")
+    }
+    if (stats[i] == "sum") {
+      stats_text <- paste("lapply(.SD, weighted.sum, w = weight, na.rm = TRUE)",
+                          stats_text, sep = ",")
+    }
+    if (stats[i] == "max") {
+      stats_text <- paste("lapply(.SD, max, na.rm = TRUE)", stats_text, sep = ",")
+    }
+
+  }
+  stats_text <- gsub("(.*),$" ,"\\1" ,stats_text)
+  stats_text <- parse(text = paste0("c(",stats_text, ")"))
+
+  #--------- parse in Data Table
+
+  # calculation variables pattern
+  cv <- paste(as.character(calc_vars), collapse  = "|")
 
   # DT <- DT[, lapply(.SD, weighted.mean, na.rm = TRUE),
   #    .SDcols = patterns(cv),
-  #    keyby = .(surveyid, get(colvar), get(rowvar))]
-
-  DT <- DT[, lapply(.SD, weighted.mean, na.rm = TRUE),
+  #    keyby = eval(keyby_text)]
+  DT <- DT[, eval(stats_text),
      .SDcols = patterns(cv),
      keyby = eval(keyby_text)]
 
@@ -122,4 +142,6 @@ tm_DT_output <- function(countries,
   return(DT)
 }
 
-
+weighted.sum <- function(x, w = 1, na.rm = FALSE) {
+  sum(x*w, na.rm = na.rm)
+}
