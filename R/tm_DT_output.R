ba4e02465285f582382909f6c959c963ceee5e52
R.Andres Castaneda <acastanedaa@worldbank.org>
2020-01-13 21:56:05 -0500

    first functional DT output (just mean)

--- R/tm_DT_output.R
+++ R/tm_DT_output.R
@@ -1,4 +1,38 @@
-
+#' Data.Table with results of estimations for table maker
+#'
+#' @param countries
+#' @param years
+#' @param povlines
+#' @param colvar
+#' @param rowvar
+#' @param super_rowvar
+#' @param super_colvar
+#' @param calc_vars
+#' @param stats
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' DT <- tm_DT_output(countries = c("HND","HND", "PRY", "PRY"),
+#'                    years = c(2012, 2014, 2012, 2014),
+#'                    povlines = c(1.9, 3.2),
+#'                    calc_vars = c("welfare_ppp", "pov_status"))
+#'
+#' DT <- tm_DT_output(countries = c("HND","HND", "PRY", "PRY"),
+#'                    years = c(2012, 2012, 2014, 2014),
+#'                    colvar = "urban",
+#'                    rowvar = "male",
+#'                    povlines = c(1.9, 3.2),
+#'                    calc_vars = c("welfare_ppp", "pov_status"))
+#'
+#' DT <- tm_DT_output(countries = c("HND","HND", "PRY", "PRY"),
+#'                    years = c(2012, 2012, 2014, 2014),
+#'                    colvar = "urban",
+#'                    rowvar = "male",
+#'                    super_colvar = "hsize",
+#'                    povlines = c(1.9, 3.2),
+#'                    calc_vars = c("welfare_ppp", "pov_status"))
 tm_DT_output <- function(countries,
                                years        = NA  ,
                                povlines     = 1.9 ,
@@ -25,77 +59,67 @@ tm_DT_output <- function(countries,
 
   # Poverty status
   for (i in povlines) {
-    nvar <- paste0("poor_",i)
+    nvar <- paste0("pov_status",i)
     DT[, (nvar) :=  welfare_ppp < i ]
   }
 
 
   # calculation variables
   cv <- paste(as.character(calc_vars), collapse  = "|")
-  # cv <- paste0("(", cv, ")")
-  # pattern <- paste0(cv, ".*", pattern)
+
+  #--------- Summarise several columns by group
+
+  # get variable by which data will be filtered
+  keyby_var <- "list(surveyid"
+  new_names <- NULL
+  nk <- 1    # starting point. [1] == surveyid
 
 
-  # Summarise several columns by group
+  if (length(colvar) == 1) {
+    keyby_var <- paste0(keyby_var, ", get(colvar)")
+    new_names <- c(new_names, substitute(colvar))
+    nk <- nk + 1
+  }
+
+  if (length(rowvar) == 1) {
+    keyby_var <- paste0(keyby_var, ", get(rowvar)")
+    new_names <- c(new_names, substitute(rowvar))
+    nk <- nk + 1
+  }
+
+  if (length(super_colvar) == 1) {
+    keyby_var <- paste0(keyby_var, ", get(super_colvar)")
+    new_names <- c(new_names, substitute(super_colvar))
+    nk <- nk + 1
+  }
+
+  if (length(super_rowvar) == 1) {
+    keyby_var <- paste0(keyby_var, ", get(super_rowvar)")
+    new_names <- c(new_names, substitute(super_rowvar))
+    nk <- nk + 1
+  }
+  keyby_var <- paste0(keyby_var, ")")
+  keyby_text <- parse(text = keyby_var)
+
+
+  # parse in Data Table
+
+
+  # DT <- DT[, lapply(.SD, weighted.mean, na.rm = TRUE),
+  #    .SDcols = patterns(cv),
+  #    keyby = .(surveyid, get(colvar), get(rowvar))]
+
   DT <- DT[, lapply(.SD, weighted.mean, na.rm = TRUE),
      .SDcols = patterns(cv),
-     keyby = .(surveyid, get(colvar), get(rowvar))]
+     keyby = eval(keyby_text)]
+
+  # if variables besides surveyid are used in by
+  if (nk > 1) {
+    data.table::setnames(DT, 2:nk, new_names)
+  }
+
 
   return(DT)
 }
 
-
-
-
-DT <- tm_DT_output(countries = c("COL","COL", "PRY", "PRY"),
-                   years = c(2012, 2012, 2014, 2014),
-                   colvar = "urban",
-                   rowvar = "male",
-                   povlines = c(1.9, 3.2),
-                   calc_vars = c("welfare_ppp", "pov_status"))
-
-
-DT[, .(headcount = weighted.mean(poor_1.9,
-                               weight,
-                               na.rm = TRUE)),
-   keyby = .(surveyid, urban)]
-
-
-# Summarise several columns by group
-DT[, lapply(.SD, weighted.mean, na.rm = TRUE),
-    .SDcols = patterns("poor"),
-    keyby = .(surveyid, urban, male)]
-
-
-
-
-
-
-
-
-
-
-###################################################
-a <- DT[surveyid == "PRY_2014_EPH_v01_M_v02_A_GMD"]
-nvar <- paste0("poor_",1.9)
-a[, (nvar):= welfare_ppp < 1.9]
-
-
-
-
-DT1 <- readRDS("p:/03.ProjectX/data/_aux/WLD/WLD.Rds")
-
-
-
-povline <- c(1.9, 3.2)
-for (i in povlines) {
-  nvar <- paste0("poor_",i)
-  DT1[, (nvar) :=  welfare_ppp < i ]
-}
-
-
-
-DT1[, lapply(.SD, weighted.mean, na.rm = TRUE),
-    .SDcols = patterns("poor"),
-    keyby = .(surveyid, urban)]
 
