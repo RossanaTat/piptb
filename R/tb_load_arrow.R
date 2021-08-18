6015857dbe3d0e384a710f5d34546b41c31352e9
R.Andres Castaneda <acastanedaa@worldbank.org>
2021-08-17 21:51:50 -0400

    init  tb_load_arrow

---
+++ R/tb_load_arrow.R
@@ -0,0 +1,39 @@
+#' Load pre-computed  estimation of Table Maker
+#'
+#' @param country_code
+#' @param surveyid_year
+#' @param survey_acronym
+#' @param domain
+#' @param welfare_type
+#'
+#' @return
+#' @export
+#'
+#' @examples
+tb_load_arrow <-
+  function(country_code   = NULL,
+           surveyid_year  = NULL,
+           survey_acronym = NULL,
+           domain         = NULL,
+           welfare_type   = NULL) {
+
+    # on.exit ------------
+    on.exit({
+
+    })
+
+    # Defenses -----------
+    stopifnot()
+
+    # Early returns ------
+    if (TRUE) {
+      return(TRUE)
+    }
+
+    # Computations -------
+
+
+    # Return -------------
+
+
+  }
