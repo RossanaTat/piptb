c1e15acababfaf8685d071e05aff3b539392bf9f
R.Andres Castaneda <acastanedaa@worldbank.org>
2021-08-18 16:29:16 -0400

    return TRUE if success

--- R/tb_create_arrow.R
+++ R/tb_create_arrow.R
@@ -1,9 +1,9 @@
 #' Create Arrow files of precalculated table Maker indicators
 #'
-#' @param country character: vector with country codes
-#' @param year numeric: Survey years
+#' @param country_code character: vector with country codes
+#' @param surveyid_year numeric: Survey years
 #' @param survey_acronym character: Survey acronym when there is more than one
-#'   survey per year. It only works if `length(country) == 1`
+#'   survey per year. It only works if `length(country_code) == 1`
 #' @param data_level character: either "D1" for national, "D2" for urban/rural,
 #'   "D3" for subnational
 #' @param arrow_format character: either "parquet" of "feather". Former default.
@@ -16,13 +16,13 @@
 #' @export
 #'
 #' @examples
-tb_create_arrow <- function(country        = NULL,
-                            year           = NULL,
+tb_create_arrow <- function(country_code   = NULL,
+                            surveyid_year  = NULL,
                             survey_acronym = NULL,
                             data_level     = NULL,
                             welfare_type   = NULL,
                             arrow_format   = c("parquet", "feather"),
-                            root_dir       = Sys.getenv("PIP_root_dir"),
+                            root_dir       = Sys.getenv("PIP_DATA_ROOT_FOLDER"),
                             verbose        = TRUE) {
 
 
@@ -34,7 +34,8 @@ tb_create_arrow <- function(country        = NULL,
   gls <- create_globals(root_dir)
 
   # get countries in Table maker
-  inv <- fst::read_fst(paste0(gls$CACHE_SVY_TB_DIR, "_crr_inventory/crr_inventory.fst"),
+  inv <- fst::read_fst(paste0(gls$CACHE_SVY_TB_DIR,
+                              "_crr_inventory/crr_inventory.fst"),
                        as.data.table = TRUE)
 
   countries_tb <-
@@ -45,9 +46,9 @@ tb_create_arrow <- function(country        = NULL,
   # Defenses -----------
 
   # all countries are ok
-  ctr    <- !(country %in% countries_tb)
+  ctr    <- !(country_code %in% countries_tb)
   if (any(ctr)) {
-    bad_ctr <- country[ctr]
+    bad_ctr <- country_code[ctr]
     cli::cli_abort(c("{length(bad_ctr)} countr{?y/ies} {?is/are} not allowed",
 
                      "i" = "Make sure all your country codes are available in
@@ -58,17 +59,17 @@ tb_create_arrow <- function(country        = NULL,
 
   # Acronym with more than one country
 
-  if (!is.null(survey_acronym) && length(country) >1) {
+  if (!is.null(survey_acronym) && length(country_code) >1) {
 
     cli::cli_abort(c("{.var survey_acronym} can't be specified if
-                     {.code length(country) > 1}",
+                     {.code length(country_code) > 1}",
 
-                     "x" = "{.code length(country)} = {.field {length(country)}}"))
+                     "x" = "{.code length(country_code)} = {.field {length(country_code)}}"))
   }
 
 
   if (verbose) {
-    cli::cli_progress_step("working on {country}")
+    cli::cli_progress_step("working on {country_code}")
   }
 
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -76,8 +77,8 @@ tb_create_arrow <- function(country        = NULL,
   # Working directory
   arrow_dir <- paste0(gls$TB_ARROW, arrow_format, "/")
 
-  .data <- pipload::pip_load_cache(country        = country,
-                                   year           = year,
+  .data <- pipload::pip_load_cache(country        = country_code,
+                                   year           = surveyid_year,
                                    survey_acronym = survey_acronym,
                                    data_level     = data_level,
                                    welfare_type   = welfare_type,
@@ -122,6 +123,8 @@ tb_create_arrow <- function(country        = NULL,
         # group_by(country_code) %>%
         dplyr::select(-ID) %>%
         arrow::write_dataset(arrow_dir, format = arrow_format)
+
+      TRUE
     }, # end of expr section
 
     error = function(e) {
