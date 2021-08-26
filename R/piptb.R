6e68fa0346e5eb5a6e03fbc8b0cb86d3dafc8fd9
R.Andres Castaneda <acastanedaa@worldbank.org>
2021-08-26 09:41:43 -0400

    import pip_create_globals from pipload

---
+++ R/piptb.R
@@ -0,0 +1,11 @@
+#' piptb: A package for managing the table maker of PIP
+#'
+#' The piptb package allows the user to perform the calcualtions, create the
+#' tables, store the results and load the results of the Table Maker tool in PIP
+#'
+#' @section piptb functions: The piptb functions ...
+#'
+#' @docType package
+#' @name piptb
+#' @importFrom pipload pip_create_globals
+NULL
